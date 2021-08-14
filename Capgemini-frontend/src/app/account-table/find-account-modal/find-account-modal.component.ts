b6d9da5106aaaf7ebbc5e1d3a5115d5db053b02e
Sylvain <sy.durant@hotmail.com>
2021-08-14 10:17:21 +0200

    implemented find account form

--- Capgemini-frontend/src/app/account-table/find-account-modal/find-account-modal.component.ts
+++ Capgemini-frontend/src/app/account-table/find-account-modal/find-account-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter  } from '@angular/core';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-find-account-modal',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindAccountModalComponent implements OnInit {
 
-  constructor() { }
+  error: string = "";
+  @Output() passAccountEvent = new EventEmitter<object>();
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(accountNumber: string) {
+    this.error = '';
+
+    if (!accountNumber) {
+      this.error = "Please, enter a valid account number.";
+    } else {
+      this.apiService.getAccountInformations(accountNumber).subscribe((data: any)=>{
+        if (data.error) {
+          this.error = data.error;
+        } else {
+          this.passAccountEvent.emit(data);
+        }
+      }) 
+    } 
+  }
 }
